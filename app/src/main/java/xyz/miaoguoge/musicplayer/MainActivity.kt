a3cc68cb0730020c71baca62be222241e6bf9975
Hoshimgg <hoshimgg@outlook.com>
2022-05-20 15:08:09 +0800

    首页写完啦

--- app/src/main/java/xyz/miaoguoge/musicplayer/MainActivity.kt
+++ app/src/main/java/xyz/miaoguoge/musicplayer/MainActivity.kt
@@ -15,21 +15,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        setContentView(R.layout.activity_main)
     }
 }
