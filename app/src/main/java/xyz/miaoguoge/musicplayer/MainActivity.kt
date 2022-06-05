0c21191d24c2983073252969067105705d76174f
hoshimgg <hoshimgg@outlook.com>
2022-06-05 23:07:36 +0800

    播放页写完啦

--- app/src/main/java/xyz/miaoguoge/musicplayer/MainActivity.kt
+++ app/src/main/java/xyz/miaoguoge/musicplayer/MainActivity.kt
@@ -1,13 +1,16 @@
 package xyz.miaoguoge.musicplayer
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val btnPlay: Button = findViewById(R.id.btnPlay)
         val btnPause: Button = findViewById(R.id.btnPause)
         btnPlay.setOnClickListener {
@@ -17,6 +20,11 @@ class MainActivity : AppCompatActivity() {
         btnPause.setOnClickListener {
             btnPause.visibility = Button.INVISIBLE
             btnPlay.visibility = Button.VISIBLE
+        }
+
+        val rlPlayBar: RelativeLayout = findViewById(R.id.rlPlayBar)
+        rlPlayBar.setOnClickListener {
+            startActivity(Intent(this, PlayerActivity::class.java))
         }
     }
 }
