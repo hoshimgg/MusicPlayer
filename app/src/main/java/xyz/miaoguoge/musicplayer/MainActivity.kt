39ca58a7a15d506547512f3e47ae5fc101fb381a
hoshimgg <hoshimgg@outlook.com>
2022-06-05 21:43:25 +0800

    优化import

--- app/src/main/java/xyz/miaoguoge/musicplayer/MainActivity.kt
+++ app/src/main/java/xyz/miaoguoge/musicplayer/MainActivity.kt
@@ -2,19 +2,9 @@ package xyz.miaoguoge.musicplayer
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import xyz.miaoguoge.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
