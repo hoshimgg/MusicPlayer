960f397ecfb04600b3d46d6f9f1e1c10a4d72356
hoshimgg <hoshimgg@outlook.com>
2022-06-05 21:41:44 +0800

    按钮可点击

--- app/src/main/java/xyz/miaoguoge/musicplayer/MainActivity.kt
+++ app/src/main/java/xyz/miaoguoge/musicplayer/MainActivity.kt
@@ -1,6 +1,8 @@
 package xyz.miaoguoge.musicplayer
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -16,5 +18,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnPlay: Button = findViewById(R.id.btnPlay)
+        val btnPause: Button = findViewById(R.id.btnPause)
+        btnPlay.setOnClickListener {
+            btnPlay.visibility = Button.INVISIBLE
+            btnPause.visibility = Button.VISIBLE
+        }
+        btnPause.setOnClickListener {
+            btnPause.visibility = Button.INVISIBLE
+            btnPlay.visibility = Button.VISIBLE
+        }
     }
 }
