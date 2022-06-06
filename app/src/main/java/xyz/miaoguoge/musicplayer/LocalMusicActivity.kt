ff592593e7d31c540301c6a3591926d0fde9e13e
ZZH <2450024092@qq.com>
2022-06-07 00:45:47 +0800

    增加LocalMusicActivity

---
+++ app/src/main/java/xyz/miaoguoge/musicplayer/LocalMusicActivity.kt
@@ -0,0 +1,15 @@
+package xyz.miaoguoge.musicplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LocalMusicActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_local_music)
+
+        //创建fragment
+        
+
+    }
+}
