5218ab469378ac93c814d8fdac9837fd8700d521
Hoshimgg <hoshimgg@outlook.com>
2022-06-06 19:15:27 +0800

    封面

--- app/src/main/java/xyz/miaoguoge/musicplayer/MainActivity.kt
+++ app/src/main/java/xyz/miaoguoge/musicplayer/MainActivity.kt
@@ -1,26 +1,47 @@
 package xyz.miaoguoge.musicplayer
 
 import android.content.Intent
+import android.content.res.AssetFileDescriptor
+import android.content.res.AssetManager
+import android.graphics.BitmapFactory
+import android.media.MediaMetadataRetriever
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
     private val mediaPlayer = MediaPlayer()
+    private val mmr = MediaMetadataRetriever()
+
+    private lateinit var assetManager: AssetManager
+    private lateinit var fd: AssetFileDescriptor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        assetManager = assets
+        fd = assetManager.openFd("snow_halation.mp3")
         initMediaPlayer()
         val btnPlay: Button = findViewById(R.id.btnPlay)
         val btnPause: Button = findViewById(R.id.btnPause)
+        val imgCover: ImageView = findViewById(R.id.imgCover)
         btnPlay.setOnClickListener {
             btnPlay.visibility = Button.INVISIBLE
             btnPause.visibility = Button.VISIBLE
             mediaPlayer.start()
+            // 获取封面
+            mmr.setDataSource(fd.fileDescriptor, fd.startOffset, fd.length)
+            val cover = mmr.embeddedPicture
+            if (cover != null) {
+                val bitmap = BitmapFactory.decodeByteArray(cover, 0, cover.size)
+                imgCover.setImageBitmap(bitmap)
+            }
         }
         btnPause.setOnClickListener {
             btnPause.visibility = Button.INVISIBLE
@@ -35,8 +56,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initMediaPlayer() {
-        val assetManager = assets
-        val fd = assetManager.openFd("snow_halation.mp3")
         mediaPlayer.setDataSource(fd.fileDescriptor, fd.startOffset, fd.length)
         mediaPlayer.prepare()
     }
