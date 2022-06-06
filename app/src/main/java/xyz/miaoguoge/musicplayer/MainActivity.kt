c815fc49af15f85a973c05e79d386e9a36739701
Hoshimgg <hoshimgg@outlook.com>
2022-06-06 17:07:43 +0800

    放鸽！

--- app/src/main/java/xyz/miaoguoge/musicplayer/MainActivity.kt
+++ app/src/main/java/xyz/miaoguoge/musicplayer/MainActivity.kt
@@ -1,30 +1,49 @@
 package xyz.miaoguoge.musicplayer
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.Button
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val mediaPlayer = MediaPlayer()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initMediaPlayer()
         val btnPlay: Button = findViewById(R.id.btnPlay)
         val btnPause: Button = findViewById(R.id.btnPause)
         btnPlay.setOnClickListener {
             btnPlay.visibility = Button.INVISIBLE
             btnPause.visibility = Button.VISIBLE
+            mediaPlayer.start()
         }
         btnPause.setOnClickListener {
             btnPause.visibility = Button.INVISIBLE
             btnPlay.visibility = Button.VISIBLE
+            mediaPlayer.pause()
         }
 
         val rlPlayBar: RelativeLayout = findViewById(R.id.rlPlayBar)
         rlPlayBar.setOnClickListener {
             startActivity(Intent(this, PlayerActivity::class.java))
         }
+    }
+
+    private fun initMediaPlayer() {
+        val assetManager = assets
+        val fd = assetManager.openFd("snow_halation.mp3")
+        mediaPlayer.setDataSource(fd.fileDescriptor, fd.startOffset, fd.length)
+        mediaPlayer.prepare()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.stop()
+        mediaPlayer.release()
     }
 }
--- app/src/main/java/xyz/miaoguoge/musicplayer/PlayerActivity.kt
+++ app/src/main/java/xyz/miaoguoge/musicplayer/PlayerActivity.kt
@@ -2,10 +2,22 @@ package xyz.miaoguoge.musicplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
+
+        val btnPlay: Button = findViewById(R.id.btnPlay)
+        val btnPause: Button = findViewById(R.id.btnPause)
+        btnPlay.setOnClickListener {
+            btnPlay.visibility = Button.INVISIBLE
+            btnPause.visibility = Button.VISIBLE
+        }
+        btnPause.setOnClickListener {
+            btnPause.visibility = Button.INVISIBLE
+            btnPlay.visibility = Button.VISIBLE
+        }
     }
 }
