19a10bccbd2e9475be31417e4a3875493048221a
ZZH <2450024092@qq.com>
2022-06-07 14:24:51 +0800

    LocalMusicActivity可以切换四个Fragment

--- app/src/main/java/xyz/miaoguoge/musicplayer/LocalMusicActivity.kt
+++ app/src/main/java/xyz/miaoguoge/musicplayer/LocalMusicActivity.kt
@@ -1,13 +1,39 @@
 package xyz.miaoguoge.musicplayer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class LocalMusicActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_local_music)
 
+        // 得到ViewPager对象
+        //创建viewpager和tablayout
 
+
+        // 得到ViewPager对象
+        //创建viewpager和tablayout
+        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+        val musicTab = findViewById<TabLayout>(R.id.musicTab)
+        val musicFragmentPagerAdapter = MusicFragmentPageAdapter(this)
+        viewPager.adapter = musicFragmentPagerAdapter
+        viewPager.currentItem = 0
+        //创建tablayout和viewpaper的连接器
+        //创建tablayout和viewpaper的连接器
+        val tabLayoutMediator = TabLayoutMediator(
+            musicTab, viewPager
+        ) { tab: TabLayout.Tab, position: Int ->
+            when (position) {
+                0 -> tab.text = "单曲"
+                1 -> tab.text = "专辑"
+                2 -> tab.text = "歌手"
+                3 -> tab.text = "文件夹"
+            }
+        }
+        tabLayoutMediator.attach()
     }
 }
---
+++ app/src/main/java/xyz/miaoguoge/musicplayer/MusicFragmentPageAdapter.java
@@ -0,0 +1,32 @@
+package xyz.miaoguoge.musicplayer;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class MusicFragmentPageAdapter extends FragmentStateAdapter {
+    public MusicFragmentPageAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 1:
+                return new AlbumFragment();
+            case 2:
+                return new SingerFragment();
+            case 3:
+                return new FolderFragment();
+            default:
+                return new SingleSongFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+}
