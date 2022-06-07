444344c01c1459ffbbb3d46e280b6fc4014d1c04
hoshimgg <hoshimgg@outlook.com>
2022-06-07 15:50:30 +0800

    进度条控制

--- app/src/main/java/xyz/miaoguoge/musicplayer/PlayerActivity.kt
+++ app/src/main/java/xyz/miaoguoge/musicplayer/PlayerActivity.kt
@@ -4,7 +4,10 @@ import android.graphics.BitmapFactory
 import android.media.MediaMetadataRetriever
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.SeekBar
+import android.widget.Toast
 import xyz.miaoguoge.musicplayer.databinding.ActivityPlayerBinding
 
 class PlayerActivity : AppCompatActivity() {
@@ -35,13 +38,28 @@ class PlayerActivity : AppCompatActivity() {
             binding.btnPlay.visibility = Button.VISIBLE
             Config.mediaPlayer.pause()
         }
+
+        binding.seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    Log.d("PlayerActivity", "seek to $progress")
+                    val duration = Config.mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)!!.toInt()
+                    val msec = progress * duration / 100
+                    Config.mediaPlayer.seekTo(msec)
+                    val currentTime = "${msec / 1000 / 60}:${msec / 1000 % 60}"
+                    binding.tvCurrentTime.text = currentTime
+                }
+            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
     }
 
     private fun updateInfo() {
         binding.tvSongTitle.text = Config.mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE)
         binding.tvSongArtist.text = Config.mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST)
-        val duration = Config.mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
-        val endTime = "${duration!!.toInt() / 1000 / 60}:${duration.toInt() / 1000 % 60}"
+        val duration = Config.mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)!!.toInt()
+        val endTime = "${duration / 1000 / 60}:${duration / 1000 % 60}"
         binding.tvEndTime.text = endTime
         val cover = Config.mmr.embeddedPicture
         if (cover != null) {
